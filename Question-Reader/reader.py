376cf1dd0d5a3cb2ada217b03cddad0fc33ad90e
WRueter <43909364+WRueter@users.noreply.github.com>
2019-12-13 22:22:24 -0500

    Got tts working, answering cmd line working, quitting working

--- Question-Reader/reader.py
+++ Question-Reader/reader.py
@@ -1,14 +1,34 @@
 import pyttsx3
+import time
 engine = pyttsx3.init()
 engine.setProperty('rate', 120)
 
 quizletList = []
-file = open('Question-Reader\quizlet.txt', 'r')
-quizletList = file.readlines()
+file = open('Question-Reader\quiz2.txt', 'r')
+copyList = file.readlines()
 file.close()
+for i in range(0, len(copyList), 2):
+    quizletList.append([copyList[i], copyList[i+1]])
 
 for term in quizletList:
-    engine.say(quizletList[quizletList.index(term)])
+    engine.say(str(quizletList[quizletList.index(term)][0]))
     engine.runAndWait()
+    q = input('Answer? (first part only)')
+    type(q)
+    correct = True
+    for i in range(len(q)):
+        if q[i] == quizletList[quizletList.index(term)][1][i]:
+            correct = True
+        else:
+            correct = False
+            engine.say('incorrect')
+            engine.runAndWait()
+            break
+    engine.say(str(quizletList[quizletList.index(term)][1]))
+    engine.runAndWait()
+    query = input("Continue? (y/n) ")
+    type(query)
+    if query == 'n':
+        break
 
 print(len(quizletList))
